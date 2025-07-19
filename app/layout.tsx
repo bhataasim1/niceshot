120ac0833067dc67c87aae3b2a3dd880cf30c92a
bhataasim1 <bhataasim700@gmail.com>
2025-07-19 21:42:06 +0530

    Refactor import statements and update global styles for consistency
    - Changed double quotes to single quotes in import statements across multiple files for uniformity.
    - Updated font-serif definitions in `globals.css` to use single quotes for consistency.
    - Minor adjustments made to layout and button components to align with the new import style.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,21 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "March Shot",
+  title: 'March Shot',
   description:
-    "Want stunning social posts? MarchShot makes your screenshots pop in just a few clicks!",
+    'Want stunning social posts? MarchShot makes your screenshots pop in just a few clicks!',
 };
 
 export default function RootLayout({
