6f3cbe8db523b0a37de46ba150d742a51d169587
bhataasim1 <bhataasim700@gmail.com>
2025-07-24 22:11:20 +0530

    Add app icons and manifest for improved branding and SEO
    - Added new app icons including apple-icon.png, icon1.png, icon2.png, opengraph-image.png, and twitter-image.png to enhance visual branding.
    - Created a manifest.ts file to define application metadata, including name, description, and icons for better integration with web standards.
    - Updated layout.tsx to include enhanced metadata for improved SEO and social sharing capabilities.
    - Added robots.txt to manage web crawler access.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Providers from '@/components/providers/providers';
@@ -14,9 +14,60 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: 'Nice Shot',
+  metadataBase: new URL('https://nice-shot.vercel.app'),
+  title: {
+    default: 'NiceShot - Turn screenshots into beautiful visuals',
+    template: '%s | NiceShot',
+    absolute: 'NiceShot',
+  },
   description:
     'Want stunning social posts? NiceShot makes your screenshots pop in just a few clicks!',
+  openGraph: {
+    url: 'https://nice-shot.vercel.app',
+    siteName: 'NiceShot',
+  },
+  keywords: [
+    'nice-shot.vercel.app',
+    'nice shot',
+    'nice shot app',
+    'nice shot app',
+    'Photography',
+    'NiceShot',
+    'screenshot background tool',
+    'screenshot enhancer',
+    'ui mockup generator',
+    'social media mockup',
+    'nice shot app',
+    'product screenshot design',
+    'image background editor',
+    'visual content tool',
+    'design tool for startups',
+    'marketing image generator',
+    'landing page screenshots',
+    'create stunning UI shots',
+    'mockup tool for SaaS',
+    'app screenshot mockup',
+    'drag and drop mockup creator',
+    'generate product visuals',
+    'automated screenshot styling',
+    'social media post designer',
+    'no code design tool',
+    'create UI previews online',
+    'beautiful background generator',
+    'design screenshots for marketing',
+  ],
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: '#ffffff' },
+    { media: '(prefers-color-scheme: dark)', color: '#0A0A0A' },
+  ],
 };
 
 export default function RootLayout({
